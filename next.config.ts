9eb7ac7e8d6b59c5c582fdafd447be67ca5140c1
immuune <hypnosleep@googlemail.com>
2026-02-26 17:07:35 +0000

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  // good default for Vercel/Next 15 builds
-  output: "standalone",
-};
-
-export default nextConfig;
